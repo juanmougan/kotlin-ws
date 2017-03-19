4df42c794642998ee3453a0297a7490c6b832a61
Juan Manuel Mougan <juanmougan@gmail.com>
2017-03-19 01:01:35 -0300

    Some more work in the api

--- src/main/kotlin/helloWorld.kt
+++ src/main/kotlin/helloWorld.kt
@@ -1,18 +1,27 @@
 /**
+ * REST API for getting tokens
  * Created by juanma on 15/3/17.
  */
 
 import spark.Spark.get
 import spark.Spark.post
+import spark.Spark.*
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 fun main(args: Array<String>) {
     get("/hello") { req, res -> "Hello World" }
-    get("/token") { req, res ->
-        createToken()
-    }
-    post("/token") { req, res ->
-        val token = req.queryParams("token")
-        retrieveToken(token)
+    path("/tokens") {
+        get("") { req, res ->
+            createToken()
+        }
+        // TODO check this instead, so I can get the params from the body
+        // http://stackoverflow.com/a/30113863/3923525
+        post("") { req, res ->
+            val token = req.queryParams("token")
+            val verifiedToken = retrieveToken(token)
+            res.type("application/json")
+            jacksonObjectMapper().writeValueAsString(verifiedToken)
+        }
     }
 }
 
